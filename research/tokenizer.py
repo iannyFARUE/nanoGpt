13cedf366619295bfed8e0b8f6850cced3a4a270
Ian Farai Madhara <iannyfarai@gmail.com>
2026-02-23 23:59:55 -0500

    v4

--- research/tokenizer.py
+++ research/tokenizer.py
@@ -19,6 +19,14 @@ def tokenizerV3():
     result = [token for token in result if token.strip() != '']
     print(result)
 
+
+def tokenizerV4():
+    import re
+    text = "Hello, world! This-- is a test. Let's see how it works."
+    result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+    result = [token for token in result if token.strip() != '']
+    print(result)
+    
 if __name__ == "__main__":
     tokenizerV3()
 
