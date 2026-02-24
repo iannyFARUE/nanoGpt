1c5b52a593183ecf535080edc57ba9d2b9ca65c0
Ian Farai Madhara <iannyfarai@gmail.com>
2026-02-23 23:51:20 -0500

    added v1 tokenizer

--- research/tokenizer.py
+++ research/tokenizer.py
@@ -1,8 +1,14 @@
-def test():
+def tokenizerV1():
     import re
     text = "Hello, world! This is a test. Let's see how it works."
     result = re.split(r'(\s)', text)
     print(result)
 
 if __name__ == "__main__":
-    test()
+    tokenizerV1()
+
+def tokenizerV2():
+    import re
+    text = "Hello, world! This is a test. Let's see how it works."
+    result = re.split(r'([,.]|\s)', text)
+    print(result)
